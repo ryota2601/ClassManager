cf18f4c4f0affeb21f8dd5858120c2cd8da553ad
Ryuji Yasukochi <ryuji.yasu@gmail.com>
2021-02-16 18:56:31 +0900

    20210216

--- app/Http/Controllers/ToppageController.php
+++ app/Http/Controllers/ToppageController.php
@@ -2,14 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use App\Models\Lesson;
 
 class ToppageController extends Controller
 {
     //
-    public function showTimetable() 
+    public function showTimetable()
     {
-        return view('timetable.list');
+        $lessons=Lesson::all();
+
+        return view('timetable.list',array("lessons"=>$lessons));
     }
 
     public function addForm()
@@ -17,10 +21,22 @@ class ToppageController extends Controller
         return view('timetable.addForm');
     }
 
-    public function exeAdd(Request $request)
+    public function registerForm(Request $request)
     {
-        Toppage::create();
+
+        $time=$request->request->get("time");
+        $name=$request->request->get("name");
+        $day=$request->request->get("day");
+        $lesson = new Lesson();
+        $lesson->university_id=1;
+        $lesson->department_id=1;
+        $lesson->name=$name;
+        $lesson->day_id=$day;
+        $lesson->time_id=$time;
+        $lesson->start_time=new Carbon('2021-04-01');
+        $lesson->end_time=new Carbon('2021-07-31');
+        $lesson->save();
         \Session::flash('eer_msg', '授業を登録しました');
-        return redirect(route('/'));
+        return redirect()->route("top_page");
     }
 }
